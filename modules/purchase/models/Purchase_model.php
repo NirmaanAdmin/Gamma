9835144b500048e6cd2a723bdea522f4128b23b0
Pawan Kumar <happyk682@gmail.com>
2025-12-22 14:39:25 +0530

    changes

--- modules/purchase/models/Purchase_model.php
+++ modules/purchase/models/Purchase_model.php
@@ -17369,7 +17369,7 @@ class Purchase_model extends App_Model
 
             {$BU_HTML}
 
-            <p>[g] AND WHEREAS as per the terms and conditions mentioned in the said Agreement the Vendor has agreed to sell to the Purchaser and the Purchaser has agreed to purchase from Vendor the said property for a consideration of Rs.<strong>{$FINAL_AMOUNT}</strong>/- (Rupees <strong>{$FINAL_AMOUNT_WORDS} Only</strong>).</p>
+            <p>[g] AND WHEREAS as per the terms and conditions mentioned in the said Agreement the Vendor has agreed to sell to the Purchaser and the Purchaser has agreed to purchase from Vendor the said property for a consideration of Rs.<strong>{$FINAL_AMOUNT}/- (Rupees {$FINAL_AMOUNT_WORDS} Only</strong>).</p>
 
             <p>[h] THE PURCHASER has no complaint, dispute or grievance regarding amounts paid by them to THE SELLER in the matter of acquisition of the Said Premises and in all matters relating to the said Project- Scheme, its common amenities, facilities and services, in general. THE PURCHASER has been given receipts for all the amounts paid by him. No payment has been made by THE PURCHASER for which no receipt has been given. THE PURCHASER has agreed that no claim for any payment made by him shall be valid unless receipt for the same is produced - issued by THE SELLER or its agent. The payment particulars made by THE PURCHASER are as follows :-</p><br><br>
             <div class="page-break"></div>
