865557dea0e8771dca90e3f4363bc76a3cafa310
Pawan Kumar <happyk682@gmail.com>
2025-12-30 13:34:20 +0530

    changes

--- modules/purchase/models/Purchase_model.php
+++ modules/purchase/models/Purchase_model.php
@@ -16982,6 +16982,17 @@ class Purchase_model extends App_Model
         if ($sale_agreement_id == 80) {
             $PAGE_BREAK5 = '<div class="page-break"></div>';
         }
+        $REMOVE_PAGE_BREAK = 0;
+        if($sale_agreement_id == 133){
+            $REMOVE_PAGE_BREAK = 1;
+        }
+
+        if($REMOVE_PAGE_BREAK == 1){
+            $PAGE_BREAK_PYMENT = '';
+        }else{
+            $PAGE_BREAK_PYMENT = '<div class="page-break"></div>';
+        }
+
         $PAYMENT_HTML = '';
 
         if (!empty($payment_details)) {
@@ -17379,7 +17390,8 @@ class Purchase_model extends App_Model
             <p>[g] AND WHEREAS as per the terms and conditions mentioned in the said Agreement the Vendor has agreed to sell to the Purchaser and the Purchaser has agreed to purchase from Vendor the said property for a consideration of Rs.<strong>{$FINAL_AMOUNT}/- (Rupees {$FINAL_AMOUNT_WORDS} Only</strong>).</p>
 
             <p>[h] THE PURCHASER has no complaint, dispute or grievance regarding amounts paid by them to THE SELLER in the matter of acquisition of the Said Premises and in all matters relating to the said Project- Scheme, its common amenities, facilities and services, in general. THE PURCHASER has been given receipts for all the amounts paid by him. No payment has been made by THE PURCHASER for which no receipt has been given. THE PURCHASER has agreed that no claim for any payment made by him shall be valid unless receipt for the same is produced - issued by THE SELLER or its agent. The payment particulars made by THE PURCHASER are as follows :-</p><br><br>
-            <div class="page-break"></div>
+
+            {$PAGE_BREAK_PYMENT}
             {$PAYMENT_HTML}
             <p>There is no other any type of consideration for sale deed of the said Premises not appearing on record, paid or agreed to be paid by THE PURCHASER to THE SELLER.
             </p>
